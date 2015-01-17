2cb997b6584f34bc344feccee36e7fbd78158859
adriansega <adriansega@hotmail.com>
2015-01-17 20:12:44 +0100

    Sección de juegos terminada completamente

--- php/juego.php
+++ php/juego.php
@@ -6,10 +6,9 @@ $sql = "SELECT * FROM `juegos` WHERE 1";
 $resultado = mysqli_query($con,$sql);
 
 echo "<table border = '0' style=".'"'."text-align:".'center'.'"'."> \n";
-echo "<tr><td> Nombre </td><td> </td><td> Descripcion </td></tr> \n"; 
 while($rows=mysqli_fetch_array($resultado)){ 
 $image=$rows['imagenJuego'];
-echo "<tr><td><br/>".utf8_encode($rows['nombreJuego'])."<br/><br/>".'<img src="'.$image.'"'.'/>'."</td><td>     
+echo "<tr><td><br/><strong>".utf8_encode($rows['nombreJuego'])."</strong><br/><br/>".'<img src="'.$image.'"'.'/>'."</td><td>     
 &nbsp;&nbsp;&nbsp;</td><td><br/><br/><br/>".utf8_encode($rows["descripcionJuego"]);"</td></tr>"; 
 } 
 echo "</table>";
