0843fd9d25a34b7f47ec6e918be1508a22b08224
Painterman <paintermeen@gmail.com>
2015-01-22 21:42:35 +0100

    Revert "Arreglo login"
    This reverts commit 08273c88173fd1f2a799b5160e18b9118c2cdde4.

--- php/check_login.php
+++ php/check_login.php
@@ -1,22 +1,17 @@
 <?php
 include_once "conexion.php";
 	
-	//recuperar balores
 	$login=$_POST['login']; 
 	$password=$_POST['password']; 
-	//conectar base de datos
 	$con = mysqli_connect("localhost","admin","1234");
 	mysqli_select_db($con, "afim21")or die("No se tienen permisos");
-	//consulta
-	$sql = "SELECT * FROM administradores WHERE login = '$login' and password = '$password' ";
+	$sql = "SELECT * FROM administradores WHERE login = '$login' and password = '$password'";
 	$result = mysqli_query($con,$sql);
 	$count= mysqli_num_rows($result);
-	//interpretar resultado
+	
 	if($count==1){	
-	echo "Su usario es correcto. ";
-	echo "<a href='http://localhost/TecWeb/administracion.html'>Continuar</a>";
+	echo "Su usario es correcto";
 	}else{
-	echo "Su usuario es incorrecto, intente nuevamente. ";
-	echo "<a href='http://localhost/TecWeb/php/inicio_sesion.php'>VOLVER</a>";
+	echo "Su usuaridghsghso es incorrecto, intente nuevamente";
 	}
 ?>
